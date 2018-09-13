70edf891d1ee767866da17b16fca4e59f7b5b4bd
Dominicdootson <Dominic.dootson@keble.ox.ac.uk>
2018-09-13 22:01:30 +0100

    First Commit

---
+++ files_m/EDGB_eqs.m
@@ -0,0 +1,221 @@
+{{Derivative[1][B][r] + 
+   (-4 + B[r]*(4 + r^2*Derivative[1][\[Phi]][r]^2 - 4*E^\[Phi][r]*\[Alpha]*
+        (-1 + B[r])*(Derivative[1][\[Phi]][r]^2 + Derivative[2][\[Phi]][r])))/
+    (4*r - 2*E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*Derivative[1][\[Phi]][r]), 
+  Derivative[1][A][r] + 
+   (A[r]*(4 + B[r]*(-4 + r^2*Derivative[1][\[Phi]][r]^2)))/
+    (2*B[r]*(-2*r + E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*
+       Derivative[1][\[Phi]][r])), 
+  (B[r]*Derivative[1][A][r]^2*(r - E^\[Phi][r]*\[Alpha]*B[r]*
+        Derivative[1][\[Phi]][r]) - A[r]^2*(2*Derivative[1][B][r] + 
+       r*B[r]*Derivative[1][\[Phi]][r]^2) + A[r]*Derivative[1][A][r]*
+      (-(r*Derivative[1][B][r]) + B[r]*(-2 + E^\[Phi][r]*\[Alpha]*
+          Derivative[1][\[Phi]][r]*(3*Derivative[1][B][r] + 
+           2*B[r]*Derivative[1][\[Phi]][r]))))/(2*E^\[Phi][r]*\[Alpha]*A[r]*
+     B[r]^2*Derivative[1][A][r]) + 
+   ((-(r/(E^\[Phi][r]*\[Alpha]*B[r])) + Derivative[1][\[Phi]][r])*
+     Derivative[2][A][r])/Derivative[1][A][r] + Derivative[2][\[Phi]][r], 
+  (E^\[Phi][r]*\[Alpha]*Derivative[1][A][r]*
+     (-((-1 + B[r])*B[r]*Derivative[1][A][r]) + A[r]*(-1 + 3*B[r])*
+       Derivative[1][B][r]))/(2*r^2*A[r]^2*B[r]) + 
+   ((4/r + Derivative[1][A][r]/A[r] + Derivative[1][B][r]/B[r])*
+     Derivative[1][\[Phi]][r])/2 + (E^\[Phi][r]*\[Alpha]*(-1 + B[r])*
+     Derivative[2][A][r])/(r^2*A[r]) + Derivative[2][\[Phi]][r]}, 
+ {Kp[r]*(r^(-1) - Derivative[1][A][r]/(2*A[r])) + Derivative[1][Kp][r] - 
+   (r*\[Kappa]*Alm1p[r])/(Sqrt[2]*(r*\[Sigma] - E^\[Phi][r]*\[Alpha]*\[Sigma]*
+       B[r]*Derivative[1][\[Phi]][r])) + 
+   (H2[r]*(-2*r + E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*Derivative[1][\[Phi]][
+        r]))/(2*r*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   (\[Phi]1[r]*(r^3*A[r]*Derivative[1][\[Phi]][r] + E^\[Phi][r]*\[Alpha]*
+       (-1 + B[r])*(r*Derivative[1][A][r] + 
+        A[r]*(2 - 2*r*Derivative[1][\[Phi]][r]))))/
+    (2*r^3*A[r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   (E^\[Phi][r]*\[Alpha]*(-1 + B[r])*Derivative[1][\[Phi]1][r])/
+    (r^2*(-r + E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   (H1[r]*((4*\[Chi]*Derivative[1][Y][\[Chi]] + 2*(-1 + \[Chi]^2)*
+         Derivative[2][Y][\[Chi]])/Y[\[Chi]] + 
+      (-2*r*(-2 + M^2 + 2*\[Chi]^2) + r*(-1 + \[Chi]^2)*
+         (4*r*Derivative[1][B][r] + B[r]*(4 + r^2*Derivative[1][\[Phi]][r]^
+              2)) + 2*E^\[Phi][r]*\[Alpha]*(Derivative[1][\[Phi]][r]*
+           (r*(-1 + \[Chi]^2)*Derivative[1][B][r] + 
+            B[r]*(M^2 + r*(-1 + \[Chi]^2)*(-3*Derivative[1][B][r] - 
+                2*(-1 + B[r])*Derivative[1][\[Phi]][r]))) - 
+          2*r*(-1 + \[Chi]^2)*(-1 + B[r])*B[r]*Derivative[2][\[Phi]][r]))/
+       ((-1 + \[Chi]^2)*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][
+           r]))))/(4*r^2*\[Sigma]), (\[Sigma]*H2[r])/B[r] + 
+   Derivative[1][H1][r] - (E^\[Phi][r]*\[Alpha]*\[Sigma]*\[Phi]1[r]*
+     Derivative[1][B][r])/(r^2*B[r] - E^\[Phi][r]*r*\[Alpha]*B[r]^2*
+      Derivative[1][\[Phi]][r]) - 
+   (r*\[Sigma]*Kp[r]*(-2 + E^\[Phi][r]*\[Alpha]*Derivative[1][B][r]*
+       Derivative[1][\[Phi]][r] + 2*E^\[Phi][r]*\[Alpha]*B[r]*
+       (Derivative[1][\[Phi]][r]^2 + Derivative[2][\[Phi]][r])))/
+    (2*B[r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   (H1[r]*(r^2*A[r]*Derivative[1][B][r] + r*B[r]*(r*Derivative[1][A][r] - 
+        3*E^\[Phi][r]*\[Alpha]*A[r]*Derivative[1][B][r]*Derivative[1][\[Phi]][
+          r]) - E^\[Phi][r]*\[Alpha]*B[r]^2*
+       (r*Derivative[1][A][r]*Derivative[1][\[Phi]][r] + 
+        2*A[r]*(-Derivative[1][\[Phi]][r] + r*Derivative[1][\[Phi]][r]^2 + 
+          r*Derivative[2][\[Phi]][r]))))/(2*r*A[r]*B[r]*
+     (r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])), 
+  -((\[Sigma]*H1[r])/A[r]) + H0[r]*(-r^(-1) + Derivative[1][A][r]/(2*A[r])) + 
+   Derivative[1][H0][r] + (r*Derivative[1][Kp][r]*
+     (-2*A[r] + E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][A][r]*
+       Derivative[1][\[Phi]][r]))/
+    (2*A[r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   (\[Phi]1[r]*(-(r^2*A[r]*Derivative[1][\[Phi]][r]) + 
+      E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][A][r]*
+       (-2 + r*Derivative[1][\[Phi]][r])))/
+    (r^2*A[r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   (H2[r]*(3*Derivative[1][A][r] + (2*(A[r] - r*Derivative[1][A][r]))/
+       (r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])))/(2*A[r]) + 
+   (E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][A][r]*Derivative[1][\[Phi]1][r])/
+    (r^2*A[r] - E^\[Phi][r]*r*\[Alpha]*A[r]*B[r]*Derivative[1][\[Phi]][r]), 
+  (-2*\[Sigma]*H1[r])/A[r] + Derivative[1][H0][r] + 
+   (2*r^2*\[Kappa]*Almp[r])/(2*r - E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*
+      Derivative[1][\[Phi]][r]) + 
+   (H2[r]*(-2*A[r] + 3*E^\[Phi][r]*\[Alpha]*B[r]^2*Derivative[1][A][r]*
+       Derivative[1][\[Phi]][r]))/(A[r]*B[r]*
+     (-2*r + E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*Derivative[1][\[Phi]][r])) - 
+   (r*Derivative[1][Kp][r]*(2*A[r] + Derivative[1][A][r]*
+       (r - 3*E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])))/
+    (A[r]*(2*r - E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*Derivative[1][\[Phi]][
+        r])) + ((E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*Derivative[1][A][r] + 
+      r^2*A[r]*Derivative[1][\[Phi]][r])*Derivative[1][\[Phi]1][r])/
+    (r*A[r]*(2*r - E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*Derivative[1][\[Phi]][
+        r])) + (H0[r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][
+        r])*(M^2*Y[\[Chi]] - (-1 + \[Chi]^2)*
+       (2*\[Chi]*Derivative[1][Y][\[Chi]] + (-1 + \[Chi]^2)*
+         Derivative[2][Y][\[Chi]])))/(r*(-1 + \[Chi]^2)*B[r]*Y[\[Chi]]*
+     (2*r - E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*Derivative[1][\[Phi]][r])) + 
+   (\[Phi]1[r]*(Y[\[Chi]]*(-(r^2*(-1 + \[Chi]^2)*A[r]*B[r]*
+          Derivative[1][\[Phi]][r]) + E^\[Phi][r]*\[Alpha]*
+         (-2*r*\[Sigma]^2*(-1 + \[Chi]^2) + 
+          B[r]*(2*r*\[Sigma]^2*(-1 + \[Chi]^2) + Derivative[1][A][r]*
+             (-1 + M^2 + \[Chi]^2 + 3*B[r] - 3*\[Chi]^2*B[r] + 
+              r*(-1 + \[Chi]^2)*(-1 + 3*B[r])*Derivative[1][\[Phi]][r])))) - 
+      E^\[Phi][r]*\[Alpha]*(-1 + \[Chi]^2)*B[r]*Derivative[1][A][r]*
+       (2*\[Chi]*Derivative[1][Y][\[Chi]] + (-1 + \[Chi]^2)*
+         Derivative[2][Y][\[Chi]])))/(r^2*(-1 + \[Chi]^2)*A[r]*B[r]*Y[\[Chi]]*
+     (2*r - E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*Derivative[1][\[Phi]][r])) + 
+   (Kp[r]*(Y[\[Chi]]*(4*r^2*\[Sigma]^2*(-1 + \[Chi]^2) + 
+        2*(-2 + M^2 + 2*\[Chi]^2)*A[r] - E^\[Phi][r]*\[Alpha]*B[r]*
+         (4*r*\[Sigma]^2*(-1 + \[Chi]^2) + (-2 + M^2 + 2*\[Chi]^2)*
+           Derivative[1][A][r])*Derivative[1][\[Phi]][r]) - 
+      (-1 + \[Chi]^2)*(2*A[r] - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][A][r]*
+         Derivative[1][\[Phi]][r])*(2*\[Chi]*Derivative[1][Y][\[Chi]] + 
+        (-1 + \[Chi]^2)*Derivative[2][Y][\[Chi]])))/
+    (2*(-1 + \[Chi]^2)*A[r]*B[r]*Y[\[Chi]]*
+     (-2*r + E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*Derivative[1][\[Phi]][r])), 
+  (2*E^\[Phi][r]*\[Alpha]*\[Sigma]*(-1 + B[r])*B[r]*Derivative[1][H1][r])/r - 
+   (B[r]*Derivative[1][H2][r]*(E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*
+       Derivative[1][A][r] + r^2*A[r]*Derivative[1][\[Phi]][r]))/(2*r) + 
+   H1[r]*((E^\[Phi][r]*\[Alpha]*\[Sigma]*(-1 + 3*B[r])*Derivative[1][B][r])/
+      r + r*\[Sigma]*B[r]*Derivative[1][\[Phi]][r]) + 
+   Derivative[1][H0][r]*
+    (-(E^\[Phi][r]*\[Alpha]*(2*(-1 + B[r])*B[r]*Derivative[1][A][r] + 
+         A[r]*(-1 + 3*B[r])*Derivative[1][B][r]))/(2*r) - 
+     (r*A[r]*B[r]*Derivative[1][\[Phi]][r])/2) + 
+   ((B[r]*Derivative[1][A][r] + A[r]*Derivative[1][B][r])*
+     Derivative[1][\[Phi]1][r])/2 + 
+   (B[r]*Derivative[1][Kp][r]*(3*E^\[Phi][r]*\[Alpha]*Derivative[1][A][r]*
+       Derivative[1][B][r] + 2*r*A[r]*Derivative[1][\[Phi]][r] + 
+      (E^\[Phi][r]*\[Alpha]*B[r]*(-(r*Derivative[1][A][r]^2) + 
+         2*A[r]*(2*Derivative[1][A][r] + r*Derivative[2][A][r])))/(r*A[r])))/
+    2 - (E^\[Phi][r]*\[Alpha]*A[r]*(-1 + B[r])*B[r]*Derivative[2][H0][r])/r + 
+   E^\[Phi][r]*\[Alpha]*B[r]^2*Derivative[1][A][r]*Derivative[2][Kp][r] + 
+   (E^\[Phi][r]*\[Alpha]*A[r]*H0[r]*Derivative[1][B][r]*
+     (-(M^2*Y[\[Chi]]) + (-1 + \[Chi]^2)*(2*\[Chi]*Derivative[1][Y][\[Chi]] + 
+        (-1 + \[Chi]^2)*Derivative[2][Y][\[Chi]])))/
+    (2*r^2*(-1 + \[Chi]^2)*Y[\[Chi]]) - 
+   (\[Phi]1[r]*((E^\[Phi][r]*\[Alpha]*(-1 + B[r])*B[r]*Derivative[1][A][r]^2)/
+       A[r] + r*(-2*r*\[Sigma]^2 + B[r]*Derivative[1][A][r]) - 
+      E^\[Phi][r]*\[Alpha]*((-1 + 3*B[r])*Derivative[1][A][r]*
+         Derivative[1][B][r] + 2*(-1 + B[r])*B[r]*Derivative[2][A][r]) + 
+      A[r]*((-2*M^2)/(-1 + \[Chi]^2) + r*Derivative[1][B][r] + 
+        (2*(2*\[Chi]*Derivative[1][Y][\[Chi]] + (-1 + \[Chi]^2)*
+            Derivative[2][Y][\[Chi]]))/Y[\[Chi]])))/(2*r^2) + 
+   (E^\[Phi][r]*\[Alpha]*Kp[r]*
+     (-(B[r]*(Derivative[1][A][r]^2 - 2*A[r]*Derivative[2][A][r])*
+        ((-2 + M^2 + 2*\[Chi]^2)*Y[\[Chi]] - (-1 + \[Chi]^2)*
+          (2*\[Chi]*Derivative[1][Y][\[Chi]] + (-1 + \[Chi]^2)*
+            Derivative[2][Y][\[Chi]]))) + A[r]*Derivative[1][B][r]*
+       (Y[\[Chi]]*(4*r*\[Sigma]^2*(-1 + \[Chi]^2) + (-2 + M^2 + 2*\[Chi]^2)*
+           Derivative[1][A][r]) - (-1 + \[Chi]^2)*Derivative[1][A][r]*
+         (2*\[Chi]*Derivative[1][Y][\[Chi]] + (-1 + \[Chi]^2)*
+           Derivative[2][Y][\[Chi]]))))/(4*r*(-1 + \[Chi]^2)*A[r]*
+     Y[\[Chi]]) + (H2[r]*((-2*E^\[Phi][r]*\[Alpha]*\[Sigma]^2)/r - 
+      (r*B[r]*Derivative[1][A][r] + A[r]*(4*B[r] + r*Derivative[1][B][r]))*
+       Derivative[1][\[Phi]][r] + (E^\[Phi][r]*\[Alpha]*
+        ((r*B[r]*(-1 + 2*B[r])*Derivative[1][A][r]^2)/A[r] + 
+         r*Derivative[1][A][r]*Derivative[1][B][r] - 
+         4*r*B[r]^2*Derivative[2][A][r] + 
+         B[r]*(Derivative[1][A][r]*(M^2/(-1 + \[Chi]^2) - 
+             6*r*Derivative[1][B][r]) + 2*r*(\[Sigma]^2 + Derivative[2][A][
+              r])) - (B[r]*Derivative[1][A][r]*
+           (2*\[Chi]*Derivative[1][Y][\[Chi]] + (-1 + \[Chi]^2)*
+             Derivative[2][Y][\[Chi]]))/Y[\[Chi]]))/r^2 - 
+      2*r*A[r]*B[r]*Derivative[2][\[Phi]][r]))/2 + 
+   A[r]*B[r]*Derivative[2][\[Phi]1][r], 
+  (E^\[Phi][r]*\[Alpha]*\[Phi]1[r]*(A[r]*Derivative[1][A][r]*
+       Derivative[1][B][r] - B[r]*(Derivative[1][A][r]^2 - 
+        2*A[r]*Derivative[2][A][r])))/B[r]^2 + 
+   (r*A[r]*(E^\[Phi][r]*\[Alpha]*B[r]*H2[r]*Derivative[1][A][r]*
+       Derivative[1][\[Phi]][r] + A[r]*(-2*H2[r] + 
+        H0[r]*(2 - E^\[Phi][r]*\[Alpha]*(Derivative[1][B][r]*
+             Derivative[1][\[Phi]][r] + 2*B[r]*(Derivative[1][\[Phi]][r]^2 + 
+              Derivative[2][\[Phi]][r]))))))/B[r]^2, 
+  (r*\[Kappa]*Alm0p[r])/(r*A[r]*B[r] - E^\[Phi][r]*\[Alpha]*A[r]*B[r]^2*
+      Derivative[1][\[Phi]][r]) + 
+   (Derivative[1][H2][r]*(-2*r + E^\[Phi][r]*\[Alpha]*(-1 + 3*B[r])*
+       Derivative[1][\[Phi]][r]))/
+    (2*r*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   ((r^3*B[r]*Derivative[1][\[Phi]][r] - E^\[Phi][r]*\[Alpha]*
+       (-(r*Derivative[1][B][r]) + B[r]*(4 - 4*B[r] + 
+          3*r*Derivative[1][B][r] + 4*r*(-1 + B[r])*Derivative[1][\[Phi]][
+            r])))*Derivative[1][\[Phi]1][r])/
+    (2*r^3*B[r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   Derivative[2][Kp][r] + (Derivative[1][Kp][r]*
+     (r*(6*B[r] + r*Derivative[1][B][r]) - E^\[Phi][r]*\[Alpha]*B[r]*
+       (Derivative[1][\[Phi]][r]*(3*r*Derivative[1][B][r] + 
+          2*B[r]*(2 + r*Derivative[1][\[Phi]][r])) + 
+        2*r*B[r]*Derivative[2][\[Phi]][r])))/
+    (2*r*B[r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   (Kp[r]*(-((-2 + M^2 + 2*\[Chi]^2)*Y[\[Chi]]) + (-1 + \[Chi]^2)*
+       (2*\[Chi]*Derivative[1][Y][\[Chi]] + (-1 + \[Chi]^2)*
+         Derivative[2][Y][\[Chi]]))*(-2 + E^\[Phi][r]*\[Alpha]*
+       (Derivative[1][B][r]*Derivative[1][\[Phi]][r] + 
+        2*B[r]*(Derivative[1][\[Phi]][r]^2 + Derivative[2][\[Phi]][r]))))/
+    (4*r*(-1 + \[Chi]^2)*B[r]*Y[\[Chi]]*(r - E^\[Phi][r]*\[Alpha]*B[r]*
+       Derivative[1][\[Phi]][r])) - 
+   (H0[r]*(-4 + 4*B[r] + 4*r*Derivative[1][B][r] + 
+      r^2*B[r]*Derivative[1][\[Phi]][r]^2 - 2*E^\[Phi][r]*\[Alpha]*
+       ((-1 + 3*B[r])*Derivative[1][B][r]*Derivative[1][\[Phi]][r] + 
+        2*(-1 + B[r])*B[r]*(Derivative[1][\[Phi]][r]^2 + 
+          Derivative[2][\[Phi]][r]))))/
+    (4*r*B[r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   (H2[r]*((-2*(2*\[Chi]*Derivative[1][Y][\[Chi]] + (-1 + \[Chi]^2)*
+          Derivative[2][Y][\[Chi]]))/Y[\[Chi]] + 
+      (2*M^2*r - r*(-1 + \[Chi]^2)*(4*r*Derivative[1][B][r] + 
+          B[r]*(4 + r^2*Derivative[1][\[Phi]][r]^2)) - 2*E^\[Phi][r]*\[Alpha]*
+         (Derivative[1][\[Phi]][r]*(r*(-1 + \[Chi]^2)*Derivative[1][B][r] + 
+            B[r]*(M^2 + 2*r*(-1 + \[Chi]^2)*(-3*Derivative[1][B][r] + 
+                (1 - 2*B[r])*Derivative[1][\[Phi]][r]))) - 
+          2*r*(-1 + \[Chi]^2)*B[r]*(-1 + 2*B[r])*Derivative[2][\[Phi]][r]))/
+       ((-1 + \[Chi]^2)*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][
+           r]))))/(4*r^2*B[r]) + 
+   (\[Phi]1[r]*(E^\[Phi][r]*r*\[Alpha]*Derivative[1][B][r]*
+       (-((-1 + M^2 + \[Chi]^2)*Y[\[Chi]]) + r*(-1 + \[Chi]^2)*Y[\[Chi]]*
+         Derivative[1][\[Phi]][r] + (-1 + \[Chi]^2)*
+         (2*\[Chi]*Derivative[1][Y][\[Chi]] + (-1 + \[Chi]^2)*
+           Derivative[2][Y][\[Chi]])) - 2*E^\[Phi][r]*\[Alpha]*
+       (-1 + \[Chi]^2)*B[r]^2*Y[\[Chi]]*
+       (2 + r*(Derivative[1][\[Phi]][r]*(-2 + r*Derivative[1][\[Phi]][r]) + 
+          r*Derivative[2][\[Phi]][r])) + (-1 + \[Chi])*(1 + \[Chi])*B[r]*
+       Y[\[Chi]]*(-(r^3*Derivative[1][\[Phi]][r]) + E^\[Phi][r]*\[Alpha]*
+         (4 + r*(-4*Derivative[1][\[Phi]][r] + Derivative[1][B][r]*
+             (3 - 3*r*Derivative[1][\[Phi]][r]) + 
+            2*r*(Derivative[1][\[Phi]][r]^2 + Derivative[2][\[Phi]][r]))))))/
+    (2*r^4*(-1 + \[Chi]^2)*B[r]*Y[\[Chi]]*
+     (r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])) + 
+   (E^\[Phi][r]*\[Alpha]*(-1 + B[r])*Derivative[2][\[Phi]1][r])/
+    (r^2*(-r + E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r]))}}
---
+++ files_m/EDGB_eqs_axial.m
@@ -0,0 +1,56 @@
+{eqbg, {((I/2)*\[Sigma]*B[r]*Derivative[1][h1][r]*
+     (r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r]))/r + 
+   (B[r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])*
+     Derivative[2][h0][r])/(2*r) + 
+   (Derivative[1][h0][r]*(r^2*(-(B[r]*Derivative[1][A][r]) + 
+        A[r]*Derivative[1][B][r]) + E^\[Phi][r]*\[Alpha]*B[r]*
+       (Derivative[1][\[Phi]][r]*(r*B[r]*Derivative[1][A][r] + 
+          A[r]*(-3*r*Derivative[1][B][r] + B[r]*(2 - 2*r*Derivative[1][
+                 \[Phi]][r]))) - 2*r*A[r]*B[r]*Derivative[2][\[Phi]][r])))/
+    (4*r^2*A[r]) + ((I/4)*\[Sigma]*h1[r]*
+     (-(r*B[r]*Derivative[1][A][r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*
+          Derivative[1][\[Phi]][r])) + 
+      A[r]*(r*(4*B[r] + r*Derivative[1][B][r]) - E^\[Phi][r]*\[Alpha]*B[r]*
+         (3*r*Derivative[1][B][r]*Derivative[1][\[Phi]][r] + 
+          2*B[r]*(Derivative[1][\[Phi]][r] + r*Derivative[1][\[Phi]][r]^2 + 
+            r*Derivative[2][\[Phi]][r])))))/(r^2*A[r]) + 
+   (h0[r]*(r*B[r]*Derivative[1][A][r]^2*(r - E^\[Phi][r]*\[Alpha]*B[r]*
+         Derivative[1][\[Phi]][r]) + A[r]^2*(4 - 2*L - 2*L^2 - 4*B[r] - 
+        4*r*Derivative[1][B][r] - r^2*B[r]*Derivative[1][\[Phi]][r]^2 + 
+        E^\[Phi][r]*\[Alpha]*((-2 + L + L^2 + 6*B[r])*Derivative[1][B][r]*
+           Derivative[1][\[Phi]][r] + 2*B[r]*(-2 + L + L^2 + 2*B[r])*
+           (Derivative[1][\[Phi]][r]^2 + Derivative[2][\[Phi]][r]))) + 
+      A[r]*(-2*r*B[r]*(r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][
+            r])*Derivative[2][A][r] + Derivative[1][A][r]*
+         (-(r^2*Derivative[1][B][r]) + E^\[Phi][r]*\[Alpha]*B[r]*
+           (Derivative[1][\[Phi]][r]*(3*r*Derivative[1][B][r] + 
+              2*B[r]*(-1 + r*Derivative[1][\[Phi]][r])) + 
+            2*r*B[r]*Derivative[2][\[Phi]][r])))))/(4*r^2*A[r]^2), 
+  (-2*h0[r])/r + Derivative[1][h0][r] + 
+   ((I/2)*h1[r]*(r*B[r]*Derivative[1][A][r]^2*
+       (r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r]) - 
+      A[r]^2*(2*(-2 + L + L^2 + r*Derivative[1][B][r]) + 
+        r^2*B[r]*Derivative[1][\[Phi]][r]^2) + 
+      A[r]*(r*(2*r*\[Sigma]^2 - r*Derivative[1][A][r]*Derivative[1][B][r] - 
+          2*B[r]*(Derivative[1][A][r] + r*Derivative[2][A][r])) + 
+        E^\[Phi][r]*\[Alpha]*B[r]*(Derivative[1][\[Phi]][r]*
+           (-2*r*\[Sigma]^2 + Derivative[1][A][r]*(-2 + L + L^2 + 
+              3*r*Derivative[1][B][r] + 2*r*B[r]*Derivative[1][\[Phi]][r]) + 
+            2*r*B[r]*Derivative[2][A][r]) + 2*r*B[r]*Derivative[1][A][r]*
+           Derivative[2][\[Phi]][r]))))/(r*\[Sigma]*A[r]*
+     (r - E^\[Phi][r]*\[Alpha]*B[r]*Derivative[1][\[Phi]][r])), 
+  h0[r] - (I*B[r]*Derivative[1][h1][r]*(-2*A[r] + E^\[Phi][r]*\[Alpha]*B[r]*
+       Derivative[1][A][r]*Derivative[1][\[Phi]][r]))/
+    (\[Sigma]*(-2 + E^\[Phi][r]*\[Alpha]*(Derivative[1][B][r]*
+         Derivative[1][\[Phi]][r] + 2*B[r]*(Derivative[1][\[Phi]][r]^2 + 
+          Derivative[2][\[Phi]][r])))) + 
+   ((I/2)*h1[r]*(2*A[r]*(B[r]*Derivative[1][A][r] + 
+        A[r]*Derivative[1][B][r]) + E^\[Phi][r]*\[Alpha]*B[r]*
+       (Derivative[1][\[Phi]][r]*(Derivative[1][A][r]*
+           (-3*A[r]*Derivative[1][B][r] + B[r]*(Derivative[1][A][r] - 
+              2*A[r]*Derivative[1][\[Phi]][r])) - 2*A[r]*B[r]*
+           Derivative[2][A][r]) - 2*A[r]*B[r]*Derivative[1][A][r]*
+         Derivative[2][\[Phi]][r])))/(\[Sigma]*A[r]*
+     (-2 + E^\[Phi][r]*\[Alpha]*(Derivative[1][B][r]*Derivative[1][\[Phi]][
+          r] + 2*B[r]*(Derivative[1][\[Phi]][r]^2 + Derivative[2][\[Phi]][
+           r]))))}}
